7b7227002ff20bac4f5303ef514fa5d6bf2c9c16
manikv12 <mac1317@live.missouristate.edu>
2021-10-30 12:43:46 -0500

    Code Cleanup

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+// Import Dependencies
 const express = require('express')
 const app = express()
-const port = 8080
 
-app.get('/', (req, res) => {
-    res.send('Hello World! Daniel')
-})
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+// Define the Port for the app to work
+const PORT = process.env.PORT || '8080';
+
+// Express Settings
+// app.use(express.static(__dirname + "/public"));
+// app.set('views', __dirname + '/views');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+
+
+// Routes
+app.use('/', app);
+
+
+// Listner
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
